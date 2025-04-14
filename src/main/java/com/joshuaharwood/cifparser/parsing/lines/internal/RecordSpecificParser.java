4a1675c7a0c6e613cd8b855c8cf9c68de84be554
Joshua J. A. Harwood <me@joshuaharwood.com>
2025-04-14 16:05:34 +0100

    WIP

--- src/main/java/com/joshuaharwood/cifparser/parsing/lines/internal/RecordSpecificParser.java
+++ src/main/java/com/joshuaharwood/cifparser/parsing/lines/internal/RecordSpecificParser.java
@@ -1,10 +1,10 @@
 package com.joshuaharwood.cifparser.parsing.lines.internal;
 
-import com.joshuaharwood.cifparser.parsing.lines.internal.fielddefinitions.BasicScheduleExtendedFields;
 import com.joshuaharwood.cifparser.parsing.lines.model.CifRecord;
 import com.joshuaharwood.cifparser.parsing.lines.model.exceptions.RequiredPropertyMissingException;
+import com.joshuaharwood.cifparser.parsing.lines.model.exceptions.RequiredPropertyMissingException.ParserPropertyMetadata;
 import java.util.Optional;
-import java.util.function.Function;
+import java.util.function.Supplier;
 import org.jspecify.annotations.Nullable;
 
 public sealed interface RecordSpecificParser<T extends CifRecord> permits AssociationParser,
@@ -13,21 +13,6 @@ public sealed interface RecordSpecificParser<T extends CifRecord> permits Associ
   TiplocDeleteParser, TiplocInsertParser, TrailerParser {
 
   T parse(String record);
-
-  default <U> U ifPresent(@Nullable U arg,
-    Function<U, U> notNullHandler,
-    Consumer<> nullHandler) {
-    if (arg != null) {
-      return notNullHandler.apply(arg);
-    }
-
-    throw new RequiredPropertyMissingException(
-      BasicScheduleExtendedFields.ATOC_CODE.getName(),
-      record,
-      parsedValues);
-
-    return nullHandler.apply(arg);
-  }
 
   default Optional<String> ifPresent(String s) {
     if (s.isBlank()) {
@@ -48,14 +33,13 @@ public sealed interface RecordSpecificParser<T extends CifRecord> permits Associ
 
     return s.charAt(0);
   }
-  
-  default <U> U required(U arg) {
-    if (arg == null) {
-      throw new RequiredPropertyMissingException()
-    }
-    
-    return Objects.require
-  }
 
-  
+
+  default <U> U required(@Nullable U arg, Supplier<ParserPropertyMetadata> exceptionSupplier) {
+    if (arg == null) {
+      throw new RequiredPropertyMissingException(exceptionSupplier.get());
+    }
+
+    return arg;
+  }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,10 @@ import org.jspecify.annotations.NullMarked;
 
 @NullMarked
 module CIFParser {
-  requires org.jspecify;
   requires org.slf4j;
   requires one.util.streamex;
+  requires com.google.common;
+  requires org.jspecify;
   exports com.joshuaharwood.cifparser.parsing.lines;
   exports com.joshuaharwood.cifparser.parsing.files;
   exports com.joshuaharwood.cifparser.parsing.lines.model;
