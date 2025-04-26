e6fb5af747b97eb673e3c20dfbb92eed31211e37
Joshua J. A. Harwood <me@joshuaharwood.com>
2025-04-26 15:56:26 +0100

    Kill old class

--- src/main/java/com/joshuaharwood/cifparser/parsing/lines/internal/ParsedRowField.java
+++
@@ -1,7 +0,0 @@
-//package com.joshuaharwood.cifparser.parsing.lines.internal;
-//
-//import com.joshuaharwood.cifparser.parsing.lines.internal.fielddefinitions.RowField;
-//
-//public record ParsedRowField<T>(RowField<T> rowField, T value) {
-//
-//}
