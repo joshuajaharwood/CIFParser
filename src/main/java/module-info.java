4800b77c8b9681179ccb956b402e74da9268db59
Joshua J. A. Harwood <me@joshuaharwood.com>
2025-04-29 09:46:06 +0100

    Unneeded module imports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,8 +6,6 @@ module CIFParser {
   requires one.util.streamex;
   requires com.google.common;
   requires org.jspecify;
-  requires java.sql;
-  requires com.google.errorprone.annotations;
   exports com.joshuaharwood.cifparser.parsing.lines;
   exports com.joshuaharwood.cifparser.parsing.files;
   exports com.joshuaharwood.cifparser.parsing.lines.model;
